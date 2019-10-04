8301ed0fd93ca10777da928e84213c8507c267e9
Devoun <THREExPENNYco@gmail.com>
2019-10-04 05:13:53 +0000

    configuring more tests for form submission

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,7 +1,22 @@
 class GramsController < ApplicationController
-
+  #root page for the application 
   def index 
   end 
+  #saves a Gram to the database and redirects back to index 
+  def create 
+    @gram = Gram.create(gram_params)
+    redirect_to root_path 
+  end 
+  #creates a new Gram object 
+  def new 
+    @gram = Gram.new
+  end 
 
+
+  private 
+
+  def gram_params 
+    params.require(:gram).permit(:message)
+  end 
 
 end
