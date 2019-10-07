7eba648d387e25785b62661735315b36a2906e1c
Devoun <THREExPENNYco@gmail.com>
2019-10-07 06:28:16 +0000

    altered :grams to include :user_id

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,11 +1,18 @@
 class GramsController < ApplicationController
-  #root page for the application 
+  # root page for the application 
   def index 
   end 
-  #saves a Gram to the database and redirects back to index 
+  # saves a Gram to the database and redirects back to index 
   def create 
     @gram = Gram.create(gram_params)
-    redirect_to root_path 
+    # validates the gram to make sure it is passing validation 
+    if @gram.valid? 
+      redirect_to root_path
+    else 
+      # returns page Unauthorized because of http status" 
+      @gram.errors.messages
+      render plain: "#{@gram.errors.messages}", status: :unprocessable_entity
+    end 
   end 
   #creates a new Gram object 
   def new 
